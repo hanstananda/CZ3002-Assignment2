12785f4b3ec7d568aa4bc63d64209320166216bd
Hans Tananda <hanstananda@gmail.com>
2020-11-19 02:05:30 +0800

    change the db to use hashed password, update sql

--- src/main/java/cz3002/group43/utils/PasswordHasher.java
+++ src/main/java/cz3002/group43/utils/PasswordHasher.java
@@ -1,11 +1,17 @@
 package cz3002.group43.utils;
 
+import com.opensymphony.xwork2.util.logging.Logger;
+import com.opensymphony.xwork2.util.logging.LoggerFactory;
+import cz3002.group43.actions.LoginAction;
+
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class PasswordHasher {
+    private static final Logger LOG = LoggerFactory.getLogger(LoginAction.class);
+
     public static String hashString(String input)
     {
         try {
@@ -13,6 +19,7 @@ public class PasswordHasher {
 
             // calculate message digest of the input string
             byte[] messageDigest = md.digest(input.getBytes());
+            LOG.debug(Base64.getEncoder().encodeToString(messageDigest));
 
             return Base64.getEncoder().encodeToString(messageDigest);
         }
--- src/main/resources/Database_Generator.sql
+++ src/main/resources/Database_Generator.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE `cz3002`;
+use `cz3002`;
 CREATE TABLE `user_table` (
   `user` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
+  `password` varchar(100) NOT NULL,
   PRIMARY KEY (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+INSERT INTO cz3002.user_table (user, password) VALUES ('farhan', 'bTjTCuqR1xjgH1xnvkCONgsHNx2KDN94cqXoN7E/AsDzcmTuFjkeb0K+MgaC2jdmOYG558NMupKMi4eMlSIG3Q==');
+INSERT INTO cz3002.user_table (user, password) VALUES ('hans', '6difygY52BLovHuoZ4CsYKEYmnO//TcCddB/TBPcNwSxeIIph6KDYGkgKx6qk6h3ols6nNrgBWYvygDocp3wSg==');
+INSERT INTO cz3002.user_table (user, password) VALUES ('marcus', 'KrCtjGR1BLiaOgnE81z5wCDxSuHjkOAsSFR9P40lFV1rus2rHU1CeXhFGbscLh9JRKUnlujSAE2Z8MwnWC0Tqg==');
+INSERT INTO cz3002.user_table (user, password) VALUES ('test', '7iaw3Ur350mqGo7jwQrpkj9hiYB3Lkc/iBml1JQODbJ6wYX4oOHV+E+IvIh/1nsUNzLDBMxfqa2Ob1f1ACio/w==');
