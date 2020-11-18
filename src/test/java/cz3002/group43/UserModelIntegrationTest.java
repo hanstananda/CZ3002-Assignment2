435e78d735a48dd7fe8733cee0e4d9f74a2e9c65
Hans Tananda <hanstananda@gmail.com>
2020-11-19 01:00:23 +0800

    updated pom, connect login logic to model, fix model bug

--- src/test/java/cz3002/group43/UserModelIntegrationTest.java
+++ src/test/java/cz3002/group43/UserModelIntegrationTest.java
@@ -1,14 +1,14 @@
 package cz3002.group43;
 import com.opensymphony.xwork2.Action;
-import cz3002.group43.model.UserModel;
+import cz3002.group43.models.UserModel;
 import junit.framework.TestCase;
 
-public class UserModelTest extends  TestCase {
+public class UserModelIntegrationTest extends  TestCase {
 
     public void testSQLTestingSuccess() throws Exception {
         UserModel action = new UserModel();
-        action.setUser("FARHAN");
-        action.setPassword("FARHAN");
+        action.setUser("test");
+        action.setPassword("test");
         String result = action.execute();
         assertEquals(Action.SUCCESS, result);
     }
